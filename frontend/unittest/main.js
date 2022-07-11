6de3353d988431f2e8b2221efd84cd692032f7d7
fuadamirullah <fuadlimpoer@gmail.com>
2022-07-11 17:31:39 +0700

    coba2

---
+++ frontend/unittest/main.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+module.exports = sum;
+    return 0; // TODO: replace this
+
+
+function multiply(a, b) {
+    return 0; // TODO: replace this
+}
+
+module.exports = {
+    sum,
+    multiply
+}
